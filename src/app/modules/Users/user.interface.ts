d07aaa83888d4559a4cc66f11c9217b3e230441c
Farukul Islam <farukgb1999@gmail.com>
2023-11-26 23:58:12 +0600

    initial setup and user interface created

---
+++ src/app/modules/Users/user.interface.ts
@@ -0,0 +1,31 @@
+import { Model } from 'mongoose';
+export type TOrder = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+export type TUser = {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+  orders?: TOrder[];
+  isDeleted: boolean;
+};
+
+export interface UserModel extends Model<TUser> {
+  isUserExists(id: string): Promise<TUser | null>;
+}
